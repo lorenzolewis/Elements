677ad177ddba1070b94eff07f8d7e27a560caaa0
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2020-06-02 12:27:55 -0500

    Initial Commit

---
+++ Elements/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Elements
+//
+//  Created by Lorenzo Lewis on 6/2/20.
+//  Copyright © 2020 Lorenzo Lewis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
